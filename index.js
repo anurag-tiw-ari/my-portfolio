8ee77cb4de656e602fde406b1445c7c73ad09fe2
anurag-tiw-ari <tiwanurag44@gmail.com>
2024-07-03 17:56:48 +0530

    new theme change till projects

--- index.js
+++ index.js
@@ -3,4 +3,16 @@ let togcircle=toggle.querySelector(".circle");
 
 togcircle.addEventListener("click",()=>{
     document.body.classList.toggle("active");
+
+    if(document.body.classList.contains("active"))
+        {
+            alert("Batman Theme activated");
+            document.querySelector(".hero-subheading").innerHTML="I AM"
+        }
+        else
+        {
+            alert("Professional Theme activated");
+            document.querySelector(".hero-subheading").innerHTML="NAMASKAR, MAIN"
+        }
+        
 })
